b9e307d20aaa31bcd7e298c700530de9878daf72
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2024-01-14 20:39:52 -0500

    Add "addItem()" to example/items store

--- src/store/index.js
+++ src/store/index.js
@@ -7,8 +7,27 @@ const storeName = 'example/items';
 
 const defaultState = [];
 
-const actions = {};
-const reducer = () => [];
+const actions = {
+  addItem(item) {
+    return {
+      type: 'ADD_ITEM',
+      item,
+    };
+  }
+};
+const reducer = (
+  state = defaultState,
+  action
+) => {
+  switch(action.type) {
+    case 'ADD_ITEM':
+      return [
+        ...state,
+        action.item
+      ];
+  }
+  return state;
+};
 const selectors = {
   getItems( state ) {
     return state;
